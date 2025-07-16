4a9230be49af5faa08b128af94bb031e8d2c9927
ria8651 <bkit865@aucklanduni.ac.nz>
2025-07-17 11:12:12 +1200

    dc settings page

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PyQt6.QtWidgets import *
 from PyQt6.uic import loadUi
 
 class Gui(QDialog):
+    @host_only
     def __init__(self, device):
         super(Gui, self).__init__()
         self.device = device
@@ -14,14 +15,17 @@ class Gui(QDialog):
         # to see what this does you can run `pyuic6 gui.ui | code -`
         loadUi('gui.ui', self)
 
-        self.submit.clicked.connect(self.submit_function)
+        self.gui_variables = ["digital", "analog", "rf_magnitude", "rf_freq"]
+        for var in self.gui_variables:
+            getattr(self, var).setValue(getattr(self.device, var))
+            getattr(self, var).valueChanged.connect(self.update_dc)
 
-    def submit_function(self):
-        self.textdisplay.setText(f"Input 1: {self.input1.text()}\nInput 2: {self.input2.text()}")
-        self.device.pulse()
+    def update_dc(self):
+        for var in self.gui_variables:
+            setattr(self.device, var, getattr(self, var).value())
 
-    def pulse(self):
-        self.device.pulse()
+        self.device.update_dc()
+
 
 class Device(EnvExperiment):
     def build(self):
@@ -30,64 +34,43 @@ class Device(EnvExperiment):
         self.setattr_device('fastino0')
         self.setattr_device('urukul0_ch0')
 
+        self.digital = 0
+        self.analog = 0.0
+        self.rf_magnitude = 1.0
+        self.rf_freq = 10.0  # in MHz
+
+    @host_only
     def run(self):
+        self.init_device()
+
         app = QApplication([])
         gui = Gui(self)
         gui.show()
         app.exec()
 
-        self.pulse()
-        
-
     @kernel
-    def pulse(self):
-        print("Switched context")
-
+    def init_device(self):
         self.core.reset()
         self.fastino0.init()
         self.urukul0_ch0.cpld.init()
         self.urukul0_ch0.init()
-
-        # pulse ttl (digital)
-        self.ttl5.on()
-
-        # signal on urukul dds (rf)
         self.urukul0_ch0.cfg_sw(True)
-        self.urukul0_ch0.set(np.pi * MHz)
-        self.urukul0_ch0.set_att(6.0*dB)
+        self.urukul0_ch0.set_att(6.0 * dB)
 
-        # ramp on fastino (analog)
-        samples = 256
-        duration = 100 * ms
-        for i in range(samples):
-            self.fastino0.set_dac(0, i / samples)
+        self.update_dc()
 
-            delay(duration / samples)
+    @kernel
+    def update_dc(self):
+        self.core.break_realtime()
 
-        self.ttl5.off()
-        self.urukul0_ch0.cfg_sw(False)
+        # update digital output
+        if self.digital:
+            self.ttl5.on()
+        else:
+            self.ttl5.off()
 
-        # self.core.wait_until_mu(now_mu())
-        print("Queued pulse!")
+        # update analog output
+        self.fastino0.set_dac(0, self.analog)
 
-    # def prepare(self):
-    #     self.period = 0.1 * s
-    #     self.sample = 128
-    #     t = np.linspace(0, 1, self.sample)
-    #     self.voltages = 4 * sp.signal.sawtooth(2 * np.pi * t, 0.5)
-    #     self.interval = self.period/self.sample
-    
-    # @kernel
-    # def run(self):
-    #     self.core.reset()
-    #     self.core.break_realtime()
-    #     self.fastino0.init()
-
-    #     delay(1*ms)
-
-    #     counter = 0
-    #     while True:
-    #         self.fastino0.set_dac(0, self.voltages[counter])
-    #         self.ttl5.pulse(self.interval / 2)
-    #         counter = (counter + 1) % self.sample
-    #         delay(self.interval)
+        # update rf output
+        self.urukul0_ch0.set(self.rf_freq * MHz, amplitude=self.rf_magnitude)
